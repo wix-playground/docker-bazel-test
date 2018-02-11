5e25acc6f808c4b0fc4da194a6bd895046001293
alonbl <alonbl@wix.com>
2018-02-11 22:19:10 +0200

    test with 2 containers that talk to each other + cloudbuild.yaml

--- src/it/scala/DockerIntegrationTest.scala
+++ src/it/scala/DockerIntegrationTest.scala
@@ -1,20 +1,36 @@
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.HttpClientBuilder
 import org.specs2.mutable.SpecificationWithJUnit
+import org.specs2.specification.BeforeAfterAll
+
 import scala.sys.process._
 
-class DockerIntegrationTest extends SpecificationWithJUnit {
+class DockerIntegrationTest extends SpecificationWithJUnit with BeforeAfterAll {
+
+  val externalPort = "31337"
 
   "DockerIT" should {
     "run containerized web server and respond with 200" in {
 
-      val externalPort = "31337"
-      Process("docker run -d -p " + externalPort + ":80 --name docker_it httpd:alpine").!!
       val client = HttpClientBuilder.create().build()
       val httpGet = new HttpGet("http://localhost:" + externalPort)
       val response = client.execute(httpGet)
-      Process("docker rm -f docker_it").!!
       response.getStatusLine.getStatusCode must beEqualTo(200)
     }
+
+    "run two containers that talk to each other" in {
+
+      Process("docker run --rm --name curl --network my_test_network appropriate/curl curl http://httpd:80").!! must contain("It works!")
+    }
+  }
+
+  def beforeAll: Unit = {
+    Process("docker network create my_test_network").!!
+    Process(s"docker run --rm -d -p $externalPort:80 --name httpd --network my_test_network httpd:alpine").!!
+  }
+
+  def afterAll: Unit = {
+    Process("docker kill httpd").!!
+    Process("docker network rm my_test_network").!!
   }
 }
